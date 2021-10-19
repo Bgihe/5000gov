8f22e7f4f6e94b9000c55bfabb481ee5632190fe
stevenLai <ccs9214221@gmail.com>
2021-10-19 16:35:40 +0800

    auto update

--- crawler.py
+++ crawler.py
@@ -5,6 +5,7 @@ import json
 from git import Repo
 import os
 import ast
+import time
 
 def writeOilDict(writeDict):
     with open("/Users/steven/Desktop/公司/git/5000gov/winNumber.json", "w+") as output:
@@ -17,7 +18,6 @@ def readWinRateDict():
 
 def pushJson():
     # https://github.com/Bgihe/5000gov.git
-
 
     configDict = readWinRateDict()
     print(configDict)
@@ -36,36 +36,33 @@ def pushJson():
     print("Successful push!")
     print("Successful push!")
 
+def startLoop():
+    response = requests.get("https://vhpi.5000.gov.tw/")
+    soup = BeautifulSoup(response.text, "html.parser")
+    pattern1 = re.compile(r"var winNo1 = {(.*?)}$", re.MULTILINE | re.DOTALL)
+    pattern2 = re.compile(r"var winNo2 = {(.*?)}$", re.MULTILINE | re.DOTALL)
+    pattern3 = re.compile(r"var winNo3 = {(.*?)}$", re.MULTILINE | re.DOTALL)
+    pattern4 = re.compile(r"var winNo4 = {(.*?)}$", re.MULTILINE | re.DOTALL)
+    script = soup.find("script", text=pattern1)
 
+    allWinNoList = {'no1': {}, 'no2': {}, 'no3': {}, 'no4': {}}
+    allWinNoJson = json.dumps(allWinNoList)
 
+    res1 = json.loads("{" + pattern1.search(str(script)).group(1) + "}")
+    res2 = json.loads("{" + pattern2.search(str(script)).group(1) + "}")
+    res3 = json.loads("{" + pattern3.search(str(script)).group(1) + "}")
+    res4 = json.loads("{" + pattern4.search(str(script)).group(1) + "}")
 
-response = requests.get("https://vhpi.5000.gov.tw/")
-soup = BeautifulSoup(response.text, "html.parser")
-pattern1 = re.compile(r"var winNo1 = {(.*?)}$", re.MULTILINE | re.DOTALL)
-pattern2 = re.compile(r"var winNo2 = {(.*?)}$", re.MULTILINE | re.DOTALL)
-pattern3 = re.compile(r"var winNo3 = {(.*?)}$", re.MULTILINE | re.DOTALL)
-pattern4 = re.compile(r"var winNo4 = {(.*?)}$", re.MULTILINE | re.DOTALL)
-script = soup.find("script", text=pattern1)
+    allWinNoList['no1'] = res1
+    allWinNoList['no2'] = res2
+    allWinNoList['no3'] = res3
+    allWinNoList['no4'] = res4
+    print(allWinNoList)
 
-allWinNoList = {'no1': {}, 'no2': {}, 'no3': {}, 'no4': {}}
-allWinNoJson = json.dumps(allWinNoList)
+    writeOilDict(allWinNoList)
+    pushJson()
+    print("sleep 60")
+    time.sleep(60)
+    startLoop()
 
-# search1 = "{" + pattern1.search(str(script)).group(1) + "}"
-# search2 = "{" + pattern2.search(str(script)).group(1) + "}"
-# search3 = "{" + pattern3.search(str(script)).group(1) + "}"
-# search4 = "{" + pattern4.search(str(script)).group(1) + "}"
-
-res1 = json.loads("{" + pattern1.search(str(script)).group(1) + "}")
-res2 = json.loads("{" + pattern2.search(str(script)).group(1) + "}")
-res3 = json.loads("{" + pattern3.search(str(script)).group(1) + "}")
-res4 = json.loads("{" + pattern4.search(str(script)).group(1) + "}")
-
-allWinNoList['no1'] = res1
-allWinNoList['no2'] = res2
-allWinNoList['no3'] = res3
-allWinNoList['no4'] = res4
-print(allWinNoList)
-
-
-writeOilDict(allWinNoList)
-pushJson()
+startLoop()
